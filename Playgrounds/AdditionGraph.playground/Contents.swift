4748b899203b83808d8e6141644a3a14a8cfe183
briankthsmth <110857165+briankthsmth@users.noreply.github.com>
2023-01-13 10:52:27 -0700

    Initial commit

---
+++ Playgrounds/AdditionGraph.playground/Contents.swift
@@ -0,0 +1,37 @@
+import Foundation
+import MimicTense
+
+NSSetUncaughtExceptionHandler { exception in
+    print("Exception thrown: \(exception)")
+}
+
+let inference = try await Inference<Float> {
+    InputDataSet(batchSize: 1) {
+        InputData { Tensor<Float>([1, 1, 1]) }
+        InputData { Tensor<Float>([1, 1, 1]) }
+        InputData { Tensor<Float>([1, 1, 1]) }
+    }
+    Sequential<Float> {
+        Arithmetic<Float>(.add) {
+            Inputs {
+                Tensor<Float>(shape: [1,3])
+                Tensor<Float>(shape: [1,3])
+            }
+        }
+        Arithmetic<Float>(.add) {
+            Inputs {
+                Tensor<Float>(shape: [1, 3])
+            }
+        }
+    }
+}
+    .compile(device: .gpu)
+
+for try await outputTensors in inference.outputStream {
+    let outputTensor = outputTensors[0]
+    print(outputTensor.shape)
+    let outputVector = outputTensor.rank2Data?.joined().map { $0 } ?? []
+    print(outputVector)
+}
+
+print("The end")
---
+++ README.md
@@ -0,0 +1,4 @@
+#  MimicTense Workspace
+
+Provides a workspace file for editing MimicTense in Xcode. Also, contains playgrounds that provide examples and tutorials for using MimicTense.
+
